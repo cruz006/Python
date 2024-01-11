dfc67f1ce89a797e8f2aa5cb82e2d8dbf9355022
cruz006 <vans5364@gmail.com>
2024-01-11 14:05:34 -0800

    reorganize files and update python

--- projects/image-to-binary/convert.py
+++ projects/image-to-binary/convert.py
@@ -1,12 +1,11 @@
 #   first replace the py.webp file with the img
 #   that you want, then replace line 8 with the
 #   name you have chosen for the file. now type
-#   this:python3 convert.py > output.txt
+#   this: python3 projects/image-to-binary/convert.py>projects/image-to-binary/output.txt
 #   check output.txt for your img conversion
 #   it might add a whole new output.txt file btw
 
 from PIL import Image
-im = Image.open('py.webp')
+im = Image.open('projects/image-to-binary/py.webp')
 pixels = list(im.getdata())
 print(pixels)
-
